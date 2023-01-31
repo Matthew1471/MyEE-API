21769eca481da3b98c2f5fb23549ad4bdad681df
Matthew <5273302+Matthew1471@users.noreply.github.com>
2023-01-31 14:15:41 +0000

    Add License Header
    - Added license header.
    - Help text editors know file encoding.

--- MyEEDataGift.py
+++ MyEEDataGift.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# This file is part of MyEE-API <https://github.com/Matthew1471/MyEE-API>
+# Copyright (C) 2023 Matthew1471!
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # Support Python3 in Python2.
 from __future__ import print_function
 
